7db4d4eafc07150302ef45791a55df74ec82aadc
jaal9709 <jaal9709@gmail.com>
2024-03-19 19:01:09 -0500

    Guard agregado

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDTO } from './dto/register.dto';
 import { LoginDTO } from './dto/login.dto';
+import { AuthGuard } from './guard/auth.guard';
+// import { Request } from 'express';
+
 
 @Controller('auth')
 export class AuthController {
@@ -23,5 +26,15 @@ export class AuthController {
     ){
         return this.authService.login(loginDTO);
     }
+
+    @Get('profile')
+    @UseGuards(AuthGuard)
+    profile(
+        @Request() req,
+        
+    ){
+        return req.user;
+    }
+
 
 }
