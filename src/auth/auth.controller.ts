26d434cb3ecaf9a8303c3d311831360a418ebb22
jaal9709 <jaal9709@gmail.com>
2024-03-21 18:06:33 -0500

    enum agregados

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,10 +2,12 @@ import { Body, Controller, Get, Post, UseGuards, Request } from '@nestjs/common'
 import { AuthService } from './auth.service';
 import { RegisterDTO } from './dto/register.dto';
 import { LoginDTO } from './dto/login.dto';
-import { AuthGuard } from './guard/auth.guard';
+// import { AuthGuard } from './guard/auth.guard';
 import { Request as Rq } from 'express';
-import { Roles } from './decorators/roles.decorator';
-import { RolesGuard } from './guard/roles.guard';
+// import { Roles } from './decorators/roles.decorator';
+// import { RolesGuard } from './guard/roles.guard';
+import { Role } from './enums/rol.enum';
+import { Auth } from './decorators/auth.decorator';
 // import { Request } from 'express';
 
 interface RequestWithUser extends Rq{
@@ -36,9 +38,19 @@ export class AuthController {
         return this.authService.login(loginDTO);
     }
 
+    // @Get('profile')
+    // @Roles(Role.ADMIN) 
+    // @UseGuards(AuthGuard, RolesGuard)
+    // profile(
+    //     @Request() req:RequestWithUser,
+        
+    // ){
+    //     return this.authService.profile(req.user)
+    // }
+
+
     @Get('profile')
-    @Roles('admin') 
-    @UseGuards(AuthGuard, RolesGuard)
+    @Auth(Role.ADMIN)
     profile(
         @Request() req:RequestWithUser,
         
